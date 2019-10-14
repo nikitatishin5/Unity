335ddfc48b343ebcd89a7be4ec4c9a230b14c3fb
o-smirnov <oleg.smirnov@firstlinesoftware.com>
2019-10-14 20:12:03 +0300

    MouseObjectControl script updated

--- Assets/MouseObjectControl.cs
+++ Assets/MouseObjectControl.cs
@@ -7,16 +7,33 @@ public class MouseObjectControl : MonoBehaviour
 
     private Vector3 mOffset { get; set; }
     private float mZCoord { get; set; }
+    private HingeJoint joint { get; set; }
+
+    private void Start()
+    {
+        gameObject.AddComponent<Rigidbody>();
+    }
 
     void OnMouseDown()
     {
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+        joint = gameObject.AddComponent<HingeJoint>();
+        joint.axis = new Vector3(1,1,1);
+    }
+
+    void OnMouseUp()
+    {
+        foreach (var item in gameObject.GetComponents<HingeJoint>())
+        {
+            Destroy(item);
+        }
     }
 
     void OnMouseDrag()
     {
         transform.position = GetMouseAsWorldPoint() + mOffset;
+        joint.anchor = gameObject.transform.position;
     }
 
     private Vector3 GetMouseAsWorldPoint()
