366f2947b14063a1303a216881a588be2e72c8db
o-smirnov <oleg.smirnov@firstlinesoftware.com>
2019-10-21 20:02:39 +0300

    simpe sound script

---
+++ Assets/Scripts/AudioManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerScript : MonoBehaviour {
+
+    public AudioSource AudioSource;
+
+    public AudioClip AudioClip;
+
+    public int volume;
+	// Use this for initialization
+	void Start () {
+        AudioSource.clip = AudioClip;
+        AudioSource.loop = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("AudioSource.Play()");
+            AudioSource.PlayOneShot(AudioClip, volume);
+        }
+	}
+}
