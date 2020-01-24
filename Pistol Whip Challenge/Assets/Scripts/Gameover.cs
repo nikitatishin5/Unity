5d5bf20650bd3a49c6e7d0bbd177d72923815dd3
Maldive39 <nikitatishin5@yandex.ru>
2020-01-24 21:53:48 +0300

    end

---
+++ Pistol Whip Challenge/Assets/Scripts/Gameover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameover : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("bullet"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+}
